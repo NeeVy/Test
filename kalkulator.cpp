3e4b4ce6307a252dc57951e80d9e5d09049e0fe4
Unknown <wojtek_wzi@wp.pl>
2017-01-28 12:12:36 +0100

    Dodalem menu

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
+	cout<<"K A L K U L A T O R"<<endl;
+	cout<<"1.Dodawanie"<<endl;
+	cout<<"2.Odejmowanie"<<endl;
+	cout<<"3.Mnozenie"<<endl;
+	cout<<"4.Dzielenie"<<ednl;
 	
 	return 0;
 }
